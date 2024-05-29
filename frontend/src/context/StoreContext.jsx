f66555fd02ee8f278ba98456a9fb9ae3db8afd9e
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-05-29 18:09:55 +0530

    updated backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({children}) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://reactjs-food-delivery.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
